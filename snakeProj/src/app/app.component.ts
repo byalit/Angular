43538c0b3e8b15c3b8263d62415982aa90be1149
byalit <92673028+byalit@users.noreply.github.com>
2022-02-18 17:01:10 +0100

    2.1

--- snakeProj/src/app/app.component.ts
+++ snakeProj/src/app/app.component.ts
@@ -17,6 +17,54 @@ export class AppComponent {
   public playerName = '';
   public playerEmail = '';
   public history = [];   
+
+  @ViewChild('game')
+  private _snake!: NgxSnakeComponent;
+
+
+  public foodEaten() {
+    this.score++;
+    }
+
+  public gameOver() {
+    this.time = 0;
+    this.pauseTimer();!
+    this.statusGame = "over";
+    alert('game over');
+  }
+
+  public gameStart() {
+      this.statusGame = "started";
+      this._snake.actionStart();
+      this.startTimer();!
+  }
+
+  public gameStop() {
+    this.statusGame = "paused";
+    this._snake.actionStop();
+    this.pauseTimer();!
+  }
+
+  public gameReset() {
+    this.statusGame = "ready";
+    this._snake.actionReset();
+    this.time = 0;!
+    this.display = '0';!
+    this.pauseTimer();!
+    this.clearHistory();!
+  }
+
+  public exitGame() {
+    this.time = 0;!
+    this.display = '0';!
+    this.score = 0;
+    this.history = [];
+    this.statusGame = "ready";
+    this._snake.actionReset();
+    this.registrationPage = 'registrationPage1';
+  }
+
+
 }
 
 
